794cf252f6d4f353fdd6ae3ed3bc9d5167837673
PongDev <pongwiwat.dev@gmail.com>
2023-04-06 14:31:00 +0700

    Add Try Catch in Auth Login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,34 +58,45 @@ exports.register = async (req, res, next) => {
 //@route   POST /api/v1/auth/login
 //@access  Public
 exports.login = async (req, res, next) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  //Validate email & password
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "Please provide an email and password" });
+    //Validate email & password
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Please provide an email and password" });
+    }
+
+    //Check for user
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Invalid credentials" });
+    }
+
+    //Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ success: false, msg: "Invalid credentials" });
+    }
+
+    //Create token
+    // const token = user.getSignedJwtToken();
+
+    // res.status(200).json({ success: true, token });
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      msg: "Cannot convert email or password to string",
+    });
   }
-
-  //Check for user
-  const user = await User.findOne({ email }).select("+password");
-
-  if (!user) {
-    return res.status(400).json({ success: false, msg: "Invalid credentials" });
-  }
-
-  //Check if password matches
-  const isMatch = await user.matchPassword(password);
-
-  if (!isMatch) {
-    return res.status(401).json({ success: false, msg: "Invalid credentials" });
-  }
-
-  //Create token
-  // const token = user.getSignedJwtToken();
-
-  // res.status(200).json({ success: true, token });
-  sendTokenResponse(user, 200, res);
 };
 
 //@desc    Get current logged in user
